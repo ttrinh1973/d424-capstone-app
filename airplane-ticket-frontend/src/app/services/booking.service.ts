281916a1e11dd723977caa3ede056577a79d4345
Toan Trinh <23277573-ttrin30@users.noreply.gitlab.com>
2026-05-22 20:58:24 -0700

    update

--- airplane-ticket-frontend/src/app/services/booking.service.ts
+++ airplane-ticket-frontend/src/app/services/booking.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError, throwError } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../environments/environment';
 
 export interface Booking {
   id?: number;
